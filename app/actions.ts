e93344121f0604fdeb61e2255b1e0a6d92a2478a
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-13 14:59:10 +0200

    test

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,14 @@
 "use server";
 
+import { headers } from "next/headers";
 import { routes } from "./data";
 
 export const checkAnswer = async (path: string, userAnswer: string) => {
+  const ip = headers().get("x-forwarded-for");
+  const allHeaders = Object.fromEntries(headers().entries());
+
+  console.log({ ip, allHeaders });
+
   const route = routes.find(route => route.pathname === path);
   if (!route) return { answerIsCorrect: false, returnHtml: "Nothing here." };
 
