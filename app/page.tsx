b9fcd1e49d84f79aa1131b44dd04d027dd65bbe5
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-14 11:12:58 +0200

    remove extra title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Title from "./components/Title";
 
 export default () => {
   return (
     <div>
-      <Title content="The Omnis Challenge" />
       <div className="text-3xl">
         Start by going{" "}
         <Link className="underline transition-colors hover:text-bloodhunt-red" href="/stages/part1-62cc0e08-a230-43e0-a7d2-4d1fd14af939">
