b5dbb33b2ff528aa122f06bc036ef9d66db8be9f
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-14 11:10:07 +0200

    not actually a part 2, just a different impl.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Title from "./components/Title";
 export default () => {
   return (
     <div>
-      <Title content="The Omnis Challenge Part 2" />
+      <Title content="The Omnis Challenge" />
       <div className="text-3xl">
         Start by going{" "}
         <Link className="underline transition-colors hover:text-bloodhunt-red" href="/stages/part1-62cc0e08-a230-43e0-a7d2-4d1fd14af939">
